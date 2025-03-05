65ac5a56d3f5d81aedc3c27dab9ece896ccb5447
Daniil Oreshenkov <danieloreshenkov@gmail.com>
2025-03-05 12:15:58 +0300

    feat: Добавлены textTabBar и imageTabBar

--- Tracker/Helpers/Resources/R + Image.swift
+++ Tracker/Helpers/Resources/R + Image.swift
@@ -2,6 +2,9 @@ import UIKit
 
 extension R {
     enum ImagesYP {
-        
+        enum TabBar {
+            static let tracker = UIImage(resource: .trackerYP)
+            static let statistic = UIImage(resource: .statisticYP)
+        }
     }
 }
---
+++ Tracker/Helpers/Resources/R + Text.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension R {
+    enum Text {
+        enum TabBar {
+            static let tracker = "Трекеры"
+            static let statistic = "Статистика"
+        }
+    }
+}
