c3de32a475fc5df9e9ceb52417b1e3df189520d5
Daniil Oreshenkov <danieloreshenkov@gmail.com>
2025-03-17 08:09:17 +0300

    feat(ui): Созданы кнопки базовые main и cancel

---
+++ Tracker/Modules/TabBar/TabBarController.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+final class TabBarController: UITabBarController {
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupAppearance()
+        setupTabBar()
+    }
+    
+    // MARK: - Private Methods
+    private func setupAppearance() {
+        view.backgroundColor = R.ColorYP.whiteDynamic
+        tabBar.isTranslucent = false
+        tabBar.tintColor = R.ColorYP.blue
+        tabBar.addTopBorder(color: R.ColorYP.gray, thickness: 0.5)
+    }
+    
+    private func setupTabBar() {
+        let trackersVC = TrackerViewController()
+        let trackersNavCon = TrackerNavigationController(rootViewController: trackersVC)
+        trackersNavCon.tabBarItem = UITabBarItem(
+            title: R.Text.tracker,
+            image: R.ImagesYP.TabBar.tracker,
+            selectedImage: nil
+        )
+        
+        let statistic = UIViewController()
+        let statisticNav = UINavigationController(rootViewController: statistic)
+        statisticNav.tabBarItem = UITabBarItem(
+            title: R.Text.statistic,
+            image: R.ImagesYP.TabBar.statistic,
+            selectedImage: nil
+        )
+        
+        viewControllers = [trackersNavCon, statisticNav]
+    }
+}
+
+
